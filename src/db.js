a47319a3fcc4de523f3fe29e9d41d6ebad21b3ca
x1vaan <ivanx467@gmail.com>
2022-12-05 20:01:30 -0300

    error db

--- src/db.js
+++ src/db.js
@@ -2,33 +2,7 @@ require('dotenv').config();
 const { Sequelize } = require('sequelize');
 const fs = require('fs');
 const path = require('path');
-const {
-  DB_USER, DB_PASSWORD, DB_HOST,DB_NAME} = process.env;
-let sequelize =
-  process.env.NODE_ENV === "production"
-    ? new Sequelize({
-        database: DB_NAME,
-        dialect: "postgres",
-        host: DB_HOST,
-        port: 5432,
-        username: DB_USER,
-        password: DB_PASSWORD,
-        pool: {
-          max: 3,
-          min: 1,
-          idle: 10000,
-        },
-        dialectOptions: {
-          ssl: {
-            require: true,
-            // Ref.: https://github.com/brianc/node-postgres/issues/2009
-            rejectUnauthorized: false,
-          },
-          keepAlive: true,
-        },
-        ssl: true,
-      })
-    : new Sequelize(
+ let sequelize = new Sequelize(
         `postgres://xiapihky:0Raqd6ULIYr2pmQsbkdLbTaY0G8bU0ZA@babar.db.elephantsql.com/xiapihky`,
         { logging: false, native: false }
       );
